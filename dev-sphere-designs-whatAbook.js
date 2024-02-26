07c1c7e79bbaaf64246ce884ba706059a70cd154
jb830 <jbrumfield623@gmail.com>
2024-02-26 12:05:54 -0800

    readme.md modified, queries js file added

---
+++ dev-sphere-designs-whatAbook.js
@@ -0,0 +1,9 @@
+/*
+============================================
+; Title:  dev-sphere-designs-whatAbook.js
+; Author: Professor Krasso
+; Date: 26, February, 2024
+; Modified by: 
+; Description: WhatABook Database Queries
+;===========================================
+*/
