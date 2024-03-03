6f8f3b87482f6eb606462de395020b82b26b5a6f
CSSkelton <codysskelton@gmail.com>
2024-03-03 07:38:46 -0500

    queries

--- dev-sphere-designs-whatAbook.js
+++ dev-sphere-designs-whatAbook.js
@@ -2,8 +2,55 @@
 ============================================
 ; Title:  dev-sphere-designs-whatAbook.js
 ; Author: Professor Krasso
-; Date: 26, February, 2024
-; Modified by: 
+; Date: 03.03.2024
+; Modified by: Cody Skelton
 ; Description: WhatABook Database Queries
 ;===========================================
 */
+
+// display entire book collection
+db.books.find()
+
+// display book collection by genre
+db.books.aggregate([
+    { $sort: { genre: 1 }}
+])
+
+// display book collection by author
+db.books.aggregate([
+    { $sort: { author: 1 }}
+])
+
+// display book collection by bookId
+db.books.aggregate([
+    { $sort: { bookId: 1 }}
+])
+
+// display wishlist by customerId
+db.customers.aggregate([
+    { $sort: { username: 1}}
+])
+
+// add books to customer's wishlist
+db.customers.aggregate([
+    {
+        $match: {
+            username: "username"
+        }
+    },
+    {
+        $lookup: {
+            from: "books",
+            localField: "wish_list.book_name",
+            foreignField: "book_name",
+            as: "book_docs"
+        }
+    },
+    {
+        $merge: {
+            
+        }
+    }
+])
+
+// remove book from customer's wishlist
