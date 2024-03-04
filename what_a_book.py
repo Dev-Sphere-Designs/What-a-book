58b1e33b22e4cec9bdbfb8f2d899481e7ecd98e0
jb830 <jbrumfield623@gmail.com>
2024-03-04 11:55:22 -0800

    made queries into functions, imported pprint

--- what_a_book.py
+++ what_a_book.py
@@ -13,7 +13,7 @@
 from pymongo import MongoClient
 import certifi
 from bson import ObjectId
-import json
+import pprint
 
 ca = certifi.where()
 
@@ -25,25 +25,37 @@ client = MongoClient("mongodb+srv://web335_admin:s3cret@whatabookdb.ff1ouoz.mong
 # substitute own database name here
 db = client['whatABookDB']
 
-# Display a list of books. Format the output so it is easy to read.
-print("Complete Book List: ")
-for books in db.books.find():
-  print(json.dumps(books, indent=3, default=str))
+# 1. Display a list of books. Format the output so it is easy to read.
+
+def display_book_collection():
+  print("Complete Book List: ")
+  for books in db.books.find():
+    pprint.pprint(books)
+    print("\n")
   
-# Display a list of books by Genre. For this requirement, supply the user with a list of genre choices 
+# 2. Display a list of books by Genre. For this requirement, supply the user with a list of genre choices 
 # and display the appropriate books based on their selection
 # Want to figure out how to do search with $or since some genres in collection have two keywords ('Action and Adventure', etc) 
-genre = input("Enter a Genre (e.g., 'Fiction', 'Fantasy', 'Action and Adventure'):")
-print("Results for:", genre.capitalize())
-for books in db.books.find({"genre": {"$regex": f'^{genre}$', "$options": "i"}}):
-  print(json.dumps(books, indent=3, default=str))
 
-# Write a query to display a wishlist by customerId.
+def display_by_genre():
+  genre = input("Enter a Genre (e.g., 'Fiction', 'Fantasy', 'Action and Adventure'): ")
+  print("Results for:", genre.capitalize())
+  for books in db.books.find({"genre": {"$regex": f'^{genre}$', "$options": "i"}}):
+    pprint.pprint(books)
+    print("\n")
+
+# 3. Write a query to display a wishlist by customerId.
 # For this requirement, prompt the user to enter a customerId (c1007, c1008, or c1009) and display the appropriate wishlist.
 # Add basic error handling to account for an invalid customerId 
 
-# Write a query to add a book to a customer’s wishlist.
+# 4. Write a query to add a book to a customer’s wishlist.
 
-# Write a query to remove a book from a customer’s wishlist. 
+# 5. Write a query to remove a book from a customer’s wishlist. 
 
 # start mainline here
+def main():
+  
+  display_book_collection()
+  display_by_genre()
+  
+main()
